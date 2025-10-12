e4703c3abb6e78afb79fdcb69c950a2d60e6a135
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2025-10-13 01:02:53 +0200

    Set sha512 and needed dependencies for a package.

--- portfile.cmake
+++ portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO JakubSzwedowicz/Utils
         REF ${VERSION}
-        SHA512 0
+        SHA512 bf5eb1bfef74e42a402f80bceae2da7e0e516c8364f6a9c989b8bb57c07e293747c838e9ea1dfc4b3bdc938c751bd9aa79e6132f22e5604d0251438f39504846
         HEAD_REF main
 )
 
